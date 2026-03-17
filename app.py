b99bba0bb7051418fdc66273881d615ab32b4710
Yawar Nazir <yawar2756@gmail.com>
2026-03-17 16:11:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ def export_csv():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+        app.run(host="0.0.0.0", port=10000)
